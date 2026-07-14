d40146f4f8e4bca573c3681a8f9d9a5c0ee3ed53
Mayank <Mayankparmar007>
2026-07-14 21:56:11 +0530

    initial commit

---
+++ pair_Sum_zero.cpp
@@ -0,0 +1,53 @@
+// Problem: Given an array of integers, find two elements whose sum is closest to zero.
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the pair of elements whose sum is closest to zero
+
+// Example:
+// Input:
+// 5
+// 1 60 -10 70 -80
+
+// Output:
+// -10 1
+
+// Explanation: Among all possible pairs, the sum of -10 and 1 is -9, which is the closest to zero compared to other pairs. 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+  vector<int> arr(n);
+  for(int i=0; i<n; ++i){
+    cin>>arr[i];
+  }
+  sort(arr.begin(),arr.end());
+  int left=0, right=n-1;
+    int closesum=INT_MAX;
+    int result_left=arr[0], result_right=arr[1];
+    while(left<right){
+        int sum = arr[left]+arr[right];
+        if(abs(sum)<abs(closesum)){
+            closesum = sum;
+            result_left=arr[left];
+            result_right=arr[right];
+        }
+
+        if(sum<0){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+      cout<<result_left<<" "<<result_right<<endl;
+    return 0;
+}
